9962a31bc0e974f00a68434acfa6d64fef0616b2
Kunal <ghanshyamjyg1@gmail.com>
2022-01-27 14:56:43 +0530

    secondCommit

---
+++ springcore/src/main/java/com/springcore/auto/wire/annotation/Emp.java
@@ -0,0 +1,42 @@
+package com.springcore.auto.wire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Emp {
+	
+	@Autowired
+	@Qualifier("address2")
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(Address address) {
+		System.out.println("inside setter");
+		this.address = address;
+	}
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+
+	
+	public Emp(Address address) {
+		super();
+		System.out.println("inside constructor");
+		
+		this.address = address;
+	}
+	
+	
+
+}
