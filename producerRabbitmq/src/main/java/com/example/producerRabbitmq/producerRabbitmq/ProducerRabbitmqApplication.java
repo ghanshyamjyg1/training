76690f2401d9483e8fb54dc27e38ba590925fe5b
ghanshyamjyg1 <ghanshyamjyg1@gmail.com>
2022-01-27 14:38:46 +0530

    completed

--- producerRabbitmq/src/main/java/com/example/producerRabbitmq/producerRabbitmq/ProducerRabbitmqApplication.java
+++ producerRabbitmq/src/main/java/com/example/producerRabbitmq/producerRabbitmq/ProducerRabbitmqApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProducerRabbitmqApplication {
-
+//nsdnjksn
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerRabbitmqApplication.class, args);
 	}
